7ef34cccef0030f51576ea94b9e327bc5a523628
VinoLoser <vinowang921@gmail.com>
2020-07-01 04:28:10 -0400

    Init Drivetrain Motors

--- Group1 - SixtyNinePointFour/src/main/java/com/stuypulse/frc/robot/subsystems/Drivetrain.java
+++ Group1 - SixtyNinePointFour/src/main/java/com/stuypulse/frc/robot/subsystems/Drivetrain.java
@@ -7,7 +7,10 @@
 
 package com.stuypulse.frc.robot.subsystems;
 
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.stuypulse.frc.Constants;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -18,9 +21,26 @@ public class Drivetrain extends SubsystemBase {
   CANSparkMax middleRightMotor;
   CANSparkMax bottomLeftMotor;
   CANSparkMax bottomRightMotor;
+
+  CANSparkMax[] leftMotors;
+  CANSparkMax[] rightMotors;
+
+  CANEncoder leftEncoder;
+  CANEncoder rightEncoder;
   //i think the rev robotics docs broke
   public Drivetrain() {
+    leftMotors = new CANSparkMax[] {
+      topleftMotor = new CANSparkMax(, MotorType.kBrushless);
+      middleleftMotor = new CANSparkMax(, MotorType.kBrushless);
+      bottomleftMotor = new CANSparkMax(, MotorType.kBrushless);
+    
+    rightMotors = new CANSparkMax[] {
+      toprightMotor = new CANSparkMax(, MotorType.kBrushless);
+      middlerightMotor = new CANSparkMax(, MotorType.kBrushless);
+      bottomrightMotor = new CANSparkMax(, MotorType.kBrushless);
 
+    leftEncoder = new CANEncoder();
+    rightEncoder = new CANEncoder();
   }
 
   @Override
