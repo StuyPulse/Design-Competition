ce981fd342882e18643c984566176ebadab2d836
Myles Pasetsky <mp3blue99@gmail.com>
2020-06-30 23:25:39 -0400

    Add DoNothingAuton

---
+++ Group4/src/main/java/com/stuypulse/frc/robot/commands/autons/DoNothingAuton.java
@@ -0,0 +1,18 @@
+package com.stuypulse.frc.robot.commands.autons;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+/**
+ * Classic first auton.
+ *
+ * This auton does literally nothing.
+ */
+public class DoNothingAuton extends SequentialCommandGroup {
+
+    public DoNothingAuton() {
+        addCommands(
+            // add commands
+        );
+    }
+
+}
