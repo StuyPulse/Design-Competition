7aeb721bdc5cfa2d3b93b9f3daa719a47efaf22d
VinoLoser <vinowang921@gmail.com>
2020-07-09 17:25:57 -0400

    fix up robot container

--- Group1 - SixtyNinePointFour/src/main/java/com/stuypulse/frc/robot/RobotContainer.java
+++ Group1 - SixtyNinePointFour/src/main/java/com/stuypulse/frc/robot/RobotContainer.java
@@ -7,7 +7,21 @@
 
 package com.stuypulse.frc.robot;
 
+import com.stuypulse.frc.robot.commands.ClimberClimbCommand;
+import com.stuypulse.frc.robot.commands.ConveyorConveyCommand;
+import com.stuypulse.frc.robot.commands.DrivetrainHighGearCommand;
+import com.stuypulse.frc.robot.commands.DrivetrainLowGearCommand;
 import com.stuypulse.frc.robot.commands.ExampleCommand;
+import com.stuypulse.frc.robot.commands.IndexerRotateCommand;
+import com.stuypulse.frc.robot.commands.IntakeAcquireCommand;
+import com.stuypulse.frc.robot.commands.IntakeDeacquireCommand;
+import com.stuypulse.frc.robot.commands.IntakeExtendCommand;
+import com.stuypulse.frc.robot.commands.IntakeRetractCommand;
+import com.stuypulse.frc.robot.subsystems.Climber;
+import com.stuypulse.frc.robot.subsystems.Conveyor;
+import com.stuypulse.frc.robot.subsystems.Drivetrain;
+import com.stuypulse.frc.robot.subsystems.Indexer;
+import com.stuypulse.frc.robot.subsystems.Intake;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
@@ -21,11 +35,21 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-
-
+  private final Climber climber = new Climber();
+  private final Conveyor conveyor = new Conveyor();
+  private final Drivetrain drivetrain = new Drivetrain();
+  private final Indexer indexer = new Indexer();
+  private final Intake intake = new Intake();
+  //commands (formated as: private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);)
+  private final ClimberClimbCommand climberClimbCommand = new ClimberClimbCommand(climber);
+  private final ConveyorConveyCommand ConveyorConveyCommand = new ConveyorConveyCommand(conveyor);
+  private final DrivetrainHighGearCommand drivetrainHighGearCommand = new DrivetrainHighGearCommand(drivetrain);
+  private final DrivetrainLowGearCommand drivetrainLowGearCommand = new DrivetrainLowGearCommand(drivetrain);
+  private final IndexerRotateCommand indexerRotateCommand = new IndexerRotateCommand(indexer);
+  private final IntakeAcquireCommand intakeAcquireCommand = new IntakeAcquireCommand(intake);
+  private final IntakeDeacquireCommand intakeDeacquireCommand = new IntakeDeacquireCommand(intake);
+  private final IntakeExtendCommand intakeExtendCommand = new IntakeExtendCommand(intake);
+  private final IntakeRetractCommand intakeRetractCommand = new IntakeRetractCommand(intake);
 
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
