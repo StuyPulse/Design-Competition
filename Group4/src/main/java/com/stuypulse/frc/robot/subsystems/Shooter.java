f6637ec0125933569190b58ea79744ee939bed33
Myles Pasetsky <mp3blue99@gmail.com>
2020-07-09 20:33:56 -0400

    Combine feeder into shooter
    Co-authored-by: Jinzxc <jzhang10@stuy.edu>
    Co-authored-by: ZhaoYuLL <zhaoyu0305@gmail.com>

--- Group4/src/main/java/com/stuypulse/frc/robot/subsystems/Shooter.java
+++ Group4/src/main/java/com/stuypulse/frc/robot/subsystems/Shooter.java
@@ -5,17 +5,27 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import com.stuypulse.frc.robot.Constants.kShooter;
 
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import com.revrobotics.CANEncoder;
+
 import com.stuypulse.stuylib.control.*;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
 
+    private CANSparkMax feeder;
+
     private CANSparkMax aMotor,bMotor,cMotor;
+
+    private CANEncoder aEncoder, bEncoder, cEncoder, feederEncoder;
+
+    private SpeedControllerGroup shooterMotors;
 
     private double targetRPM;
 
     private Controller shooterController;
+    private Controller feederController;
 
     public Shooter() {
 
@@ -23,9 +33,22 @@ public class Shooter extends SubsystemBase {
         bMotor = new CANSparkMax(kShooter.Ports.MOTOR_B, MotorType.kBrushless);
         cMotor = new CANSparkMax(kShooter.Ports.MOTOR_C, MotorType.kBrushless);
 
+        aEncoder = aMotor.getEncoder();
+        bEncoder = bMotor.getEncoder();
+        cEncoder = cMotor.getEncoder();
+
+        shooterMotors = new SpeedControllerGroup(aMotor, bMotor, cMotor);
+
+        feeder = new CANSparkMax(kShooter.Ports.FEEDER_MOTOR, MotorType.kBrushless);
+        feederEncoder = feeder.getEncoder();
+
         targetRPM = 0.0;
 
+        //
         shooterController = new PIDController(0,0,0);
+        feederController = new PIDController(0,0,0);
+
+        // do motor initialization here
     }
 
     public void setTargetRPM(double rpm) {
