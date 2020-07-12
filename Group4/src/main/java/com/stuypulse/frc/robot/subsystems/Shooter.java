bd52cae3eb662b609b656d3b8e88a68a530b4625
Myles Pasetsky <mp3blue99@gmail.com>
2020-07-12 00:29:14 -0400

    Remove controllers from shooter

--- Group4/src/main/java/com/stuypulse/frc/robot/subsystems/Shooter.java
+++ Group4/src/main/java/com/stuypulse/frc/robot/subsystems/Shooter.java
@@ -3,7 +3,7 @@ package com.stuypulse.frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import com.stuypulse.frc.robot.Constants.kShooter;
+import static com.stuypulse.frc.robot.Constants.kShooter.*;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import com.revrobotics.CANEncoder;
@@ -26,14 +26,11 @@ public class Shooter extends SubsystemBase {
 
     private double targetRPM;
 
-    private Controller shooterController;
-    private Controller feederController;
-
     public Shooter() {
 
-        aMotor = new CANSparkMax(kShooter.Ports.MOTOR_A, MotorType.kBrushless);
-        bMotor = new CANSparkMax(kShooter.Ports.MOTOR_B, MotorType.kBrushless);
-        cMotor = new CANSparkMax(kShooter.Ports.MOTOR_C, MotorType.kBrushless);
+        aMotor = new CANSparkMax(Ports.MOTOR_A, MotorType.kBrushless);
+        bMotor = new CANSparkMax(Ports.MOTOR_B, MotorType.kBrushless);
+        cMotor = new CANSparkMax(Ports.MOTOR_C, MotorType.kBrushless);
 
         aEncoder = aMotor.getEncoder();
         bEncoder = bMotor.getEncoder();
@@ -41,14 +38,10 @@ public class Shooter extends SubsystemBase {
 
         shooterMotors = new SpeedControllerGroup(aMotor, bMotor, cMotor);
 
-        feeder = new CANSparkMax(kShooter.Ports.FEEDER_MOTOR, MotorType.kBrushless);
+        feeder = new CANSparkMax(Ports.FEEDER_MOTOR, MotorType.kBrushless);
         feederEncoder = feeder.getEncoder();
 
         targetRPM = 0.0;
-
-        //
-        shooterController = new PIDController(0, 0, 0);
-        feederController = new PIDController(0, 0, 0);
 
         // do motor initialization here
 
@@ -86,20 +79,12 @@ public class Shooter extends SubsystemBase {
         feeder.set(value);
     }
 
+    public void stopFeeder() {
+        setFeeder(0);
+    }
+
     public void stopShooter() {
         setShooter(0);
     }
 
-    @Override
-    public void periodic() {
-
-        // do some checks if it's done ?
-        double shooterError = shooterController.update(targetRPM - getShooterRPM());
-
-        setShooter(shooterError);
-
-        double feederError = feederController.update(targetRPM - getFeederRPM());
-
-        setFeeder(feederError);
-    }
 }
