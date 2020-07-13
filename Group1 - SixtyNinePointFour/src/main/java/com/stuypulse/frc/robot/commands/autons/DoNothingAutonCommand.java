9780b10389ba098479709952f4671a05f33ea887
VinoLoser <vinowang921@gmail.com>
2020-07-12 20:00:17 -0400

    Rename Donothing Auton Command

--- Group1 - SixtyNinePointFour/src/main/java/com/stuypulse/frc/robot/commands/autons/DoNothingAutonCommand.java
+++
@@ -1,9 +0,0 @@
-package com.stuypulse.frc.robot.commands.autons;
-
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-
-public class DoNothingAutonCommand extends SequentialCommandGroup {
-    public DoNothingAutonCommand() {
-        
-    }
-}
---
+++ Group1 - SixtyNinePointFour/src/main/java/com/stuypulse/frc/robot/commands/autons/SixninePointFourAutonCommand.java
@@ -0,0 +1,9 @@
+package com.stuypulse.frc.robot.commands.autons;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class SixninePointFourAutonCommand extends SequentialCommandGroup {
+    public SixninePointFourAutonCommand() {
+        //haha we did nothing :)
+    }
+}
